2e3b993c118b1c4b6f0718f0b9d3de00db0025cb
adamgrieder <adamgrieder92@gmail.com>
2017-07-19 15:38:02 -0500

    PL

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -10,42 +10,42 @@ const rl = readline.createInterface({
 function pigLatin(word) {
   word = word.toLowerCase();
 
-  var firstLetter = word[0];
+  var ftLetter = word[0];
 
-  var vowelIndex = -1;
+  var vowel = -1;
 
-  if ((word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex) || vowelIndex === -1) {
-    vowelIndex = word.indexOf('a');
+  if ((word.indexOf('a') > -1 && word.indexOf('a') < vowel) || vowel === -1) {
+    vowel = word.indexOf('a');
   }
 
-  if ((word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex) || vowelIndex === -1) {
-    vowelIndex = word.indexOf('e');
+  if ((word.indexOf('e') > -1 && word.indexOf('e') < vowel) || vowel === -1) {
+    vowel = word.indexOf('e');
   }
 
-  if ((word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex) || vowelIndex === -1) {
-    vowelIndex = word.indexOf('i');
+  if ((word.indexOf('i') > -1 && word.indexOf('i') < vowel) || vowel === -1) {
+    vowel = word.indexOf('i');
   }
 
-  if ((word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex) || vowelIndex === -1) {
-    vowelIndex = word.indexOf('o');
+  if ((word.indexOf('o') > -1 && word.indexOf('o') < vowel) || vowel === -1) {
+    vowel = word.indexOf('o');
   }
 
-  if ((word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex) || vowelIndex === -1) {
-    vowelIndex = word.indexOf('u');
+  if ((word.indexOf('u') > -1 && word.indexOf('u') < vowel) || vowel === -1) {
+    vowel = word.indexOf('u');
   }
 
-  if ((word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex) || vowelIndex === -1) {
-    vowelIndex = word.indexOf('y');
+  if ((word.indexOf('y') > -1 && word.indexOf('y') < vowel) || vowel === -1) {
+    vowel = word.indexOf('y');
   }
 
-  var firstPart = word.slice(0, vowelIndex);
+  var ftPart = word.slice(0, vowel);
 
-  var restWord = word.slice(vowelIndex, word.length);
+  var lastWord = word.slice(vowel, word.length);
 
-  if (vowelIndex === 0) {
+  if (vowel === 0) {
     return word + 'yay';
   } else {
-    return restWord + firstPart + 'ay';
+    return lastWord + ftPart + 'ay';
   }
 
 }

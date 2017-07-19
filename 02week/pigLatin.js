70ff8a81db3c9c0a7665d4c4acc28b315251b9e1
adamgrieder <adamgrieder92@gmail.com>
2017-07-19 12:42:59 -0500

    PL

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -6,13 +6,54 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
-
+//code//
 function pigLatin(word) {
+  word = word.toLowerCase();
 
-  // Your code here
+  var firstLetter = word[0];
+
+  var vowelIndex = -1;
+
+  if ((word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('a');
+  }
+
+  if ((word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('e');
+  }
+
+  if ((word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('i');
+  }
+
+  if ((word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('o');
+  }
+
+  if ((word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('u');
+  }
+
+  if ((word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('y');
+  }
+
+  var firstPart = word.slice(0, vowelIndex);
+
+  var restWord = word.slice(vowelIndex, word.length);
+
+  if (vowelIndex === 0) {
+    return word + 'yay';
+  } else {
+    return restWord + firstPart + 'ay';
+  }
 
 }
+
+var trimSpace = str.trim('rock');
+var lowerCase = str.toLowerCase('scissors');
+
+
 
 
 function getPrompt() {

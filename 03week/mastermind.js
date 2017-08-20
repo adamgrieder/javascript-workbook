ab5fce0a80757174072d294666ed96e92bed4337
adamgrieder <adamgrieder92@gmail.com>
2017-08-20 17:04:43 -0500

    ToH

--- 03week/mastermind.js
+++ 03week/mastermind.js
@@ -1,77 +1,104 @@
 'use strict';
-
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 let board = [];
-let solution = '';
+let solution = 'abcd';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
+
 function printBoard() {
-  for (let i = 0; i < board.length; i++) {
-    console.log(board[i]);
-  }
+    for (let i = 0; i < board.length; i++) {
+        console.log(board[i]);
+    }
 }
 
 function generateSolution() {
-  for (let i = 0; i < 4; i++) {
-    const randomIndex = getRandomInt(0, letters.length);
-    solution += letters[randomIndex];
-  }
+    for (let i = 0; i < 4; i++) {
+        var randomIndex = getRandomInt(0, letters.length);
+        solution += letters[randomIndex];
+    }
 }
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
-  // your code here
-}
+function generateHint(solution, guess) {
+  var rl = 0;
+  var clp = 0;
+  var solarr = solution.split('');
+  var guesarr = guess.split('');
+
+  for (var i = 0; i < solarr.length; i++) {
+
+      if (solarr[i] === guesarr[i]) {
+       clp++;
+       solarr[i] = null; }
+    }
+  for (var i = 0; i < solarr.length; i++) {
+      var dstIndex = solarr.indexOf(guesarr[i]);
+      if (dstIndex > -1) {
+      rl++;
+      solarr[dstIndex] = null;
+      }
+      }
+      return clp + '-' + rl;
+        }
 
 function mastermind(guess) {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
-}
+  solution = 'abcd';
+     if (guess === solution) {
+       return ('You guessed it!');
+     } else if (board.length === 10) {
+       return ('Good Attempt! Try again?')
+     } else {
+       var hint = generateHint(solution, guess);
+       board.push(hint + ': ' + guess);
+       return 'Take Another Shot at it!'
+     }
+    }
+
 
 
 function getPrompt() {
-  rl.question('guess: ', (guess) => {
-    mastermind(guess);
-    printBoard();
-    getPrompt();
-  });
+    rl.question('guess: ', (guess) => {
+        printBoard();
+        getPrompt();
+    });
 }
 
 // Tests
 
 if (typeof describe === 'function') {
-  solution = 'abcd';
-  describe('#mastermind()', () => {
-    it('should register a guess and generate hints', () => {
-      mastermind('aabb');
-      assert.equal(board.length, 1);
-    });
-    it('should be able to detect a win', () => {
-      assert.equal(mastermind(solution), 'You guessed it!');
-    });
-  });
 
-  describe('#generateHint()', () => {
-    it('should generate hints', () => {
-      assert.equal(generateHint('abdc'), '2-2');
-    });
-    it('should generate hints if solution has duplicates', () => {
-      assert.equal(generateHint('aabb'), '1-1');
+    describe('#mastermind()', function() {
+        it('should register a guess and generate hints', function() {
+            solution = 'abcd';
+            mastermind('aabb');
+            assert.equal(board.length, 1);
+        });
+        it('should be able to detect a win', function() {
+            assert.equal(mastermind(solution), 'You guessed it!');
+        });
     });
 
-  });
+    describe('#generateHint()', function() {
+        it('should generate hints', function() {
+            assert.equal(generateHint('abcd', 'abdc'), '2-2');
+        });
+        it('should generate hints if solution has duplicates', function() {
+            assert.equal(generateHint('abcd', 'aabb'), '1-1');
+        });
+
+    });
 
 } else {
 
-  generateSolution();
-  getPrompt();
+    generateSolution();
+    getPrompt();
 }
--- 06week/towersOfHanoi/script.js
+++ 06week/towersOfHanoi/script.js
@@ -1,5 +1,30 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Your code here
+  let moved = {};
+
+  document.querySelectorAll('.ball').forEach((ball) => {
+    ball.addEventListener('click', (e) => {
+      moved = { target: e.target, size: e.target.clientHeight, parent: e.target.parentNode };
+      e.target.parentNode.removeChild(e.target);
+    });
+  });
+
+  document.querySelector('.container').addEventListener('click', (e) => {
+    console.log(e)
+    e.target.appendChild(moved.target);
+  });
+    document.querySelector('.container2').addEventListener('click', (e) => {
+      let last = e.target.children[e.target.children.length - 1];
+      let size = last && last.clientHeight;
+      if (!last || size > moved.size) {
+        e.target.appendChild(moved.target);
+      } else {
+        moved.parent.appendChild(moved.target);
+      }
+  });
+    document.querySelector('.container3').addEventListener('click', (e) => {
+    console.log(moved.target)
+    e.target.appendChild(moved.target);
+  });
 });

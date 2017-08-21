dc0fe37f97383977788af323e5f38090321f0f16
adamgrieder <adamgrieder92@gmail.com>
2017-08-21 17:40:59 -0500

    mm

--- 06week/towersOfHanoi/script.js
+++ 06week/towersOfHanoi/script.js
@@ -1,30 +1,40 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Your code here
   let moved = {};
 
-  document.querySelectorAll('.ball').forEach((ball) => {
-    ball.addEventListener('click', (e) => {
+  document.querySelectorAll('.block').forEach((block) => {
+    block.addEventListener('click', (e) => {
+
       moved = { target: e.target, size: e.target.clientHeight, parent: e.target.parentNode };
       e.target.parentNode.removeChild(e.target);
     });
   });
-
-  document.querySelector('.container').addEventListener('click', (e) => {
-    console.log(e)
+  document.querySelector('.container1').addEventListener('click', (e) => {
     e.target.appendChild(moved.target);
+    if (!e.target.children.length) {
+      e.target.appendChild(moved.target);
+    } else {
+      moved.parent.appendChild(moved.target);
+    }
   });
-    document.querySelector('.container2').addEventListener('click', (e) => {
-      let last = e.target.children[e.target.children.length - 1];
-      let size = last && last.clientHeight;
-      if (!last || size > moved.size) {
-        e.target.appendChild(moved.target);
-      } else {
-        moved.parent.appendChild(moved.target);
-      }
+  document.querySelector('.container2').addEventListener('click', (e) => {
+    let last = e.target.children[e.target.children.length - 1];
+    let size = last && last.clientHeight;
+    if (!last || size > moved.target) {
+      e.target.appendChild(moved.target);
+    } else {
+      moved.parent.appendChild(moved.target);
+    }
   });
-    document.querySelector('.container3').addEventListener('click', (e) => {
-    console.log(moved.target)
-    e.target.appendChild(moved.target);
+  document.querySelector('.container3').addEventListener('click', (e) => {
+    let last = e.target.children[e.target.children.length - 1];
+    let size = last && last.clientHeight;
+    if (!last || size > moved.target) {
+      e.target.appendChild(moved.target);
+    } else {
+      moved.parent.appendChild(moved.target);
+    }
   });
 });

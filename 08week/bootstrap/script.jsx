ba83790ddd974dc9ad508364eeac65e5817a90ee
adamgrieder <adamgrieder92@gmail.com>
2017-09-06 12:46:36 -0500

    HW

---
+++ 08week/bootstrap/script.jsx
@@ -0,0 +1,14 @@
+'use strict';
+
+class Page extends React.Component {
+constructor(props) {
+super(props);
+}
+
+render() {
+return (
+<div className="container">
+<div className="main-img">
+
+)
+}

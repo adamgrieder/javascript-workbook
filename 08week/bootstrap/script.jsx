f58566b64ce941d4c6f80af5d3cf9f15100acea0
adamgrieder <adamgrieder92@gmail.com>
2017-09-06 13:02:36 -0500

    updates

--- 08week/bootstrap/script.jsx
+++ 08week/bootstrap/script.jsx
@@ -9,6 +9,51 @@ render() {
 return (
 <div className="container">
 <div className="main-img">
-
+  <h1>Do What Now?</h1>
+</div>
+<div className="row">
+  <div className="col">
+    <ReactForm color ="azure" height="200" />
+    <span> Lorem ipsum dolor sit amet, consectetur laoreet id donec ultries tincindunt arcu nonment</div>
+  </div>
+  <div className="col"><span> Lorem ipsum dolor sit amet, consectetur laoreet id donec ultries tincindunt arcu nonment</div>
+</div>
+</div>
+</div>
+});
 )
 }
+class Page extends React.Component {
+constructor(props) {
+super(props);
+
+this.state = {
+  submitted: false,
+  color: this.props.color
+};
+this.submit = this.submit.bind(this);
+}
+render() {
+  if (!this.state.submitted) {
+    return (
+      <form className={this.state.color}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name"/>
+        <label htmlFor="name">Age:</label>
+        <label htmlFor="email">Email:</label>
+        <input type="text" name="email" />
+        <button type="submit" className="btn btn-primary" onClick={this.submit}What?</button>
+      </form>
+    );
+  } else {
+    return (<h1>Form Submitted</h1>);
+  }
+  }
+  submit () {
+    this.setState((state) => {
+      state.submitted = !state.submitted;
+    });
+    }
+  }
+}
+ReactDOM.render(<Page />, document.getElementById('container'));
